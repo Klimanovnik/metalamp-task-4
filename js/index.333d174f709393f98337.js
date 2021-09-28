7cc7ebf9ada6e4870d3e63c93dc69fe0dda2d05c
Klimanov Nikita <klimanovnik98@gmail.com>
2021-09-28 17:32:04 +0300

    Updates

--- js/index.333d174f709393f98337.js
+++
@@ -1,7 +0,0 @@
-(()=>{"use strict";var __webpack_modules__={"./index.scss":
-/*!********************!*\
-  !*** ./index.scss ***!
-  \********************/(__unused_webpack_module,__webpack_exports__,__webpack_require__)=>{eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack:///./index.scss?")},"./index.ts":
-/*!******************!*\
-  !*** ./index.ts ***!
-  \******************/(__unused_webpack_module,exports,__webpack_require__)=>{eval('\r\nObject.defineProperty(exports, "__esModule", ({ value: true }));\r\n__webpack_require__(/*! ./index.scss */ "./index.scss");\r\nvar User = /** @class */ (function () {\r\n    function User(_name) {\r\n        this.name = _name;\r\n    }\r\n    return User;\r\n}());\r\n// Comment\r\nvar nick = new User("Nick");\r\nvar wrap = document.getElementById("wrap");\r\nwrap.innerHTML = "Привет " + nick.name;\r\n\n\n//# sourceURL=webpack:///./index.ts?')}},__webpack_module_cache__={};function __webpack_require__(e){var _=__webpack_module_cache__[e];if(void 0!==_)return _.exports;var r=__webpack_module_cache__[e]={exports:{}};return __webpack_modules__[e](r,r.exports,__webpack_require__),r.exports}__webpack_require__.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})};var __webpack_exports__=__webpack_require__("./index.ts")})();
